ed9ee772f903d7061b4d5eca6f01c4793a16e805
Poly A. Titova <poly_titova@mail.ru>
2025-10-03 00:24:25 +0300

    Добавили корзину

---
+++ src/components/CartItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex items-center border border-slate-200 p-4 rounded-xl gap-4">
+    <img class="w-16 h-16" src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
+
+    <div class="flex flex-col flex-1">
+      <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
+
+      <div class="flex justify-between mt-2">
+        <b class="flex-1">5000 руб.</b>
+        <img class="opacity-40 hover:opacity-100 cursor-pointer transition"
+          src="/close.svg" />
+      </div>
+    </div>
+  </div>
+</template>
