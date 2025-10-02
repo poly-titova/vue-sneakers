80739e494501dfa9191a41d6b16a277ee0a33485
Poly A. Titova <poly_titova@mail.ru>
2025-10-02 23:30:31 +0300

    Добавили первые компоненты

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,23 @@
 <script setup>
+import Header from './components/Header.vue';
+import Card from './components/Card.vue';
 </script>
 
 <template>
-  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
-    <header class="flex fustify-between border-b border-slate-300 px-10 py-8">
-      <div class="flex items-center gap-4">
-        <img src="/logo.png" alt="Logo" class="w-10" />
-        <div>
-          <h2 class="text-xl font-bold uppercase">Vue Sneskers</h2>
-          <p class="text-slate-400">Магазин лучших кроссовок</p>
-        </div>
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
+    <Header />
+
+    <div class="p-10">
+      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+
+      <div class="grid grid-cols-4 gap-5">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
-
-      <ul class="flex items-center gap-10">
-        <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
-          <img src="/cart.svg" alt="Cart" />
-          <b>1205 руб.</b>
-        </li>
-
-        <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
-          <img src="/heart.svg" alt="Cart" />
-          <b>Закладки</b>
-        </li>
-
-        <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
-          <img src="/profile.svg" alt="Cart" />
-          <b>Профиль</b>
-        </li>
-      </ul>
-    </header>
-
-    <h1>Archakov Blog</h1>
+    </div>
   </div>
 </template>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="flex fustify-between border-b border-slate-200 px-10 py-8">
+    <div class="flex items-center gap-4">
+      <img src="/logo.png" alt="Logo" class="w-10" />
+      <div>
+        <h2 class="text-xl font-bold uppercase">Vue Sneskers</h2>
+        <p class="text-slate-400">Магазин лучших кроссовок</p>
+      </div>
+    </div>
+
+    <ul class="flex items-center gap-10">
+      <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+        <img src="/cart.svg" alt="Cart" />
+        <b>1205 руб.</b>
+      </li>
+
+      <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+        <img src="/heart.svg" alt="Cart" />
+        <b>Закладки</b>
+      </li>
+
+      <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+        <img src="/profile.svg" alt="Cart" />
+        <b>Профиль</b>
+      </li>
+    </ul>
+  </header>
+</template>
