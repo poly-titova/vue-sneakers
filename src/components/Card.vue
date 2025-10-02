a46e45c12cd2d47907f0020cdfa4b7b4c014accf
Poly A. Titova <poly_titova@mail.ru>
2025-10-02 23:49:07 +0300

    Добавили список

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,17 +1,41 @@
+<script setup>
+defineProps({
+  title: String,
+  imageUrl: String,
+  price: Number,
+  isFavorite: Boolean,
+  isAdded: Boolean,
+  onClickAdd: Function,
+  onClickFavorite: Function
+})
+</script>
+
 <template>
   <div
-    class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl">
-    <img src="/like-1.svg" alt="Like 1" class="absolute top-8 left-8" />
-    <img src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
-    <p class="mt-2">Мужские Кроссовки Nike Blazer Mid Suede</p>
+    class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
+  >
+    <img
+      :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'"
+      alt="Like 1"
+      class="absolute top-8 left-8"
+      @click="onClickFavorite"
+    />
+
+    <img :src="imageUrl" alt="Sneaker" />
+
+    <p class="mt-2">{{ title }}</p>
 
     <div class="flex justify-between mt-5">
       <div class="flex flex-col">
         <span class="text-slate-400">Цена:</span>
-        <b>1205 руб.</b>
+        <b>{{ price }} руб.</b>
       </div>
 
-      <img src="/plus.svg" alt="Plus" />
+      <img
+        @click="onClickAdd"
+        :src="!isAdded ? '/plus.svg' : '/checked.svg'"
+        alt="Plus"
+      />
     </div>
   </div>
 </template>
---
+++ src/components/CardList.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Card from './Card.vue';
+
+const onClickAdd = () => {
+  alert('Добавить!')
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      title="Мужские Кроссовки Nike Blazer Mid Suede"
+      imageUrl="/sneakers/sneakers-1.jpg"
+      :price="5000"
+      :isFavorite="false"
+      :onClickAdd="onClickAdd"
+      :isAdded="true"
+    />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
+</template>
